55c9aecc912cb033d0b90b5ae1261690c928d1da
n1meli <ddddmhg@gmail.com>
2025-07-17 20:56:56 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,17 +3,18 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const generateTextRoute = require('./routes/generateText');
-// Додатково: const generateImageRoute = require('./routes/generateImage');
-// Додатково: const ttsRoute = require('./routes/tts');
+const ttsRoute = require('./tts');
+const imageGenRoute = require('./imageGen');
+const textGenRoute = require('./textGen');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/generate-text', generateTextRoute);
-// app.use('/generate-image', generateImageRoute);
-// app.use('/tts', ttsRoute);
+app.use('/api/tts', ttsRoute);
+app.use('/api/image', imageGenRoute);
+app.use('/api/story', textGenRoute);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
