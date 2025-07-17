bddda87b7d9d977587adba106e82323a7a2840cb
n1meli <ddddmhg@gmail.com>
2025-07-17 21:05:33 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
-
-const ttsRoute = require('./tts');
-const imageGenRoute = require('./imageGen');
-const textGenRoute = require('./textGen');
+const edgeTTS = require('./edge-tts');
+const openai = require('./openai');
+const huggingface = require('./huggingface');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api/tts', ttsRoute);
-app.use('/api/image', imageGenRoute);
-app.use('/api/story', textGenRoute);
+app.post('/api/generate-story', openai.generateStory);
+app.post('/api/generate-image', huggingface.generateImage);
+app.post('/api/generate-voice', edgeTTS.generateVoice);
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
-});
+app.listen(3001, () => console.log('Server listening on http://localhost:3001'));
